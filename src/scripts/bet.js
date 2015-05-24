8b8e6aadff8531604b8d45d9be25395c5762b1af
Nick Yahnke <nthitz@gmail.com>
2015-05-23 21:57:57 -0700

    something of a start

---
+++ src/scripts/bet.js
@@ -0,0 +1,29 @@
+// map of our data keys, useful names as keys, the verbose actual data keys as the values of the object
+var keys = {
+  'payout': 'Credit',
+  'cost': 'Debit',
+  'time': 'Date/Time(PT)',
+  'betType': 'Pool',
+  'raceNumber': 'Race',
+  'selection': 'Selection',
+  'transactionType': 'TransactionType',
+  'id': 'Txn#',
+  'track': 'Track',
+  'stake': 'Stake'
+}
+
+/**
+* Returns a object with `keys` as keys and an array of unique values for each key as the values
+*/
+function createDataDictionary(data, keys) {
+  return _.mapValues(keys, function(dataKey, keyKey) {
+    return _.unique( _.pluck(data, dataKey))
+  })
+}
+
+d3.csv('xbstatement.csv', function(err, data) {
+  var bets = _.filter(data, _.matches({ key.transactionType: 'Bet'}))
+  var dd = createDataDictionary(data, keys);
+  console.log(dd)
+
+})
